48f577a406c762d42d6418a339897a20edba1fdf
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-24 17:28:39 +0200

    feat: functions review

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -14,9 +14,34 @@ function multiply(
   return firstNumber * base;
 }
 
-const result = addNumbers(1, 2);
-const resultArrow = addNumbersArrow(2, 3);
-const multiplyResult: number = multiply(5);
-console.table({ result, resultArrow, multiplyResult });
+// const result = addNumbers(1, 2);
+// const resultArrow = addNumbersArrow(2, 3);
+// const multiplyResult: number = multiply(5);
+
+// console.table({ result, resultArrow, multiplyResult });
+
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const obiWan: Character = {
+  name: 'Obi Wan Kenobi',
+  hp: 1000,
+  showHp() {
+    console.log(`${this.hp} health points`);
+  },
+};
+
+const healChar = (character: Character, amount: number) => {
+  character.hp += amount;
+};
+
+obiWan.showHp();
+
+healChar(obiWan, 1000);
+
+obiWan.showHp();
 
 export {};
