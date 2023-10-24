f03c0b1c2d02fb7521ab8e9eac50b3d48afa7028
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-24 19:34:41 +0200

    feat: class composition

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,17 +1,30 @@
 export class Person {
-  constructor(public name: string, private address: string = 'No Address') {}
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    private address: string = 'No Address'
+  ) {}
 }
 
-export class Hero extends Person {
+// export class Hero extends Person {
+//   constructor(
+//     public alterEgo: string,
+//     public age: number,
+//     public realName: string
+//   ) {
+//     super(realName, 'New York');
+//   }
+// }
+export class Hero {
   constructor(
     public alterEgo: string,
     public age: number,
-    public realName: string
-  ) {
-    super(realName, 'New York');
-  }
+    public realName: string,
+    public person: Person
+  ) {}
 }
 
-const ironman = new Hero('Ironman', 45, 'Tony');
+const tony = new Person('Tony', 'Stark', 'New York');
+const ironman = new Hero('Ironman', 45, 'Tony', tony);
 
 console.log(ironman);
