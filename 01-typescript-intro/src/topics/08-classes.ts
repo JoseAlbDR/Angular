f780aab8411c3d67aefc60e43a0bfd941d2e0f57
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-24 19:28:21 +0200

    feat: classes inheritance

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,10 +1,17 @@
 export class Person {
-  constructor(public name: string, private address: string) {
-    this.name = name;
-    this.address = address;
+  constructor(public name: string, private address: string = 'No Address') {}
+}
+
+export class Hero extends Person {
+  constructor(
+    public alterEgo: string,
+    public age: number,
+    public realName: string
+  ) {
+    super(realName, 'New York');
   }
 }
 
-const ironman = new Person('Pepe', 'Mi calle');
+const ironman = new Hero('Ironman', 45, 'Tony');
 
 console.log(ironman);
