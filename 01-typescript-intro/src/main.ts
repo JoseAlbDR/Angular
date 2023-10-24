7c255ec6d14784e6acbcfc348f1860c48b3bcec6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-24 18:28:33 +0200

    feat: homework destructuring done

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,8 +2,9 @@ import './style.css';
 // import './topics/01-basic-types';
 // import './topics/02-objects-interfaces';
 // import './topics/03-functions';
-import './topics/04-homework-types';
-import './topics/05-basic-destructuring';
+// import './topics/04-homework-types';
+// import './topics/05-basic-destructuring';
+import './topics/06-function-destructuring';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,40 @@
+interface Product {
+  description: string;
+  price: number;
+}
+
+const phone: Product = {
+  description: 'Nokia A1',
+  price: 150.0,
+};
+
+const tablet: Product = {
+  description: 'iPad Air',
+  price: 250.0,
+};
+
+interface TaxCalcOptions {
+  products: Product[];
+  tax: number;
+}
+
+const taxCalc = ({ products, tax }: TaxCalcOptions): [number, number] => {
+  let total = 0;
+
+  products.forEach(({ price }) => (total += price));
+
+  return [total, total * tax];
+};
+
+const shoppingCart: Product[] = [phone, tablet];
+const tax = 0.15;
+
+const options: TaxCalcOptions = {
+  products: shoppingCart,
+  tax,
+};
+
+const [total, taxes] = taxCalc(options);
+console.log(total, taxes);
+
+export {};
