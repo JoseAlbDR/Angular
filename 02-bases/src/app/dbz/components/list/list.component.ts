e0b13be22bc79320f1d17d2d66e64d644dcfcc7d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-26 00:14:05 +0200

    feat: @Input decorator

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent {}
+export class ListComponent {
+  @Input()
+  public charList: Character[] = [];
+}
--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,7 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html',
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    { name: 'Goku', power: 9500 },
+    { name: 'Vegeta', power: 7500 },
+  ];
+}
