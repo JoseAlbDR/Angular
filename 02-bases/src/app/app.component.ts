b453c20362b18a1ef614e3d38a13f635064f6acd
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-25 10:28:08 +0200

    feat: initial counter

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basis';
+  public title: string = 'My First Angular APP';
+  public counter: number = 10;
+  public value: number = 1;
+
+  increaseCounter(): void {
+    this.counter += this.value;
+  }
+  decreaseCounter(): void {
+    this.counter -= this.value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
