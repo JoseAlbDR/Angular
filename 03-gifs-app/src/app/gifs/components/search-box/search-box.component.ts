671b6a0c69a8541e346418f788fb0dc3b3afb19b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-27 00:52:45 +0200

    feat: search-box component

---
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrls: ['./search-box.component.css'],
+})
+export class SearchBoxComponent {}
--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, SearchBoxComponent],
   imports: [CommonModule],
+  exports: [HomePageComponent],
 })
 export class GifsModule {}
