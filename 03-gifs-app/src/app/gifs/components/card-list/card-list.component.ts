124cab0ba8d78569635f35421155393660ec2ce8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-27 00:53:55 +0200

    feat: gifs-card-list component

---
+++ 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.css'],
+})
+export class CardListComponent {}
--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent],
+  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
   imports: [CommonModule],
   exports: [HomePageComponent],
 })
