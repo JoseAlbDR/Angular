ec44d9b9f6c295fdfab9329448f4b7739c434a81
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-26 11:22:43 +0200

    feat: eventEmitter in add-form.component and listen for event in main-page.component

--- 02-bases/src/app/dbz/components/add-form/add-form.component.ts
+++ 02-bases/src/app/dbz/components/add-form/add-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -7,12 +7,20 @@ import { Character } from '../../interfaces/character.interface';
   styleUrls: ['./add-form.component.css'],
 })
 export class AddFormComponent {
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
   public character: Character = {
     name: '',
     power: 0,
   };
 
   emitCharacter(): void {
-    console.log(this.character);
+    if (this.character.name.length === 0 || this.character.power < 0) return;
+
+    this.onNewCharacter.emit(this.character);
+
+    this.character.name = '';
+    this.character.power = 0;
   }
 }
--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -14,4 +14,8 @@ export class MainPageComponent {
     { name: 'Goku', power: 9500 },
     { name: 'Vegeta', power: 7500 },
   ];
+
+  public onNewCharacter(character: Character): void {
+    this.characters.push({ ...character });
+  }
 }
