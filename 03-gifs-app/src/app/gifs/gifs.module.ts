cefdc7be5c543b082c737d4a780840a25eaeee3b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-27 00:32:41 +0200

    feat: home-page component

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomePageComponent } from './pages/home/home-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomePageComponent],
+  imports: [CommonModule],
 })
-export class GifsModule { }
+export class GifsModule {}
---
+++ 03-gifs-app/src/app/gifs/pages/home/home-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+})
+export class HomePageComponent {}
