5e7af6948ab30b21185c44cccae9cef00cfc7838
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-27 16:29:01 +0200

    feat: render gifs in card-list

--- 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
+++ 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interface';
 
 @Component({
   selector: 'gifs-card-list',
   templateUrl: './card-list.component.html',
 })
-export class CardListComponent {}
+export class CardListComponent {
+  @Input()
+  public gifs: Gif[] = [];
+}
--- 03-gifs-app/src/app/gifs/pages/home/home-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/home/home-page.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gifs.interface';
 
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  constructor(private gifService: GifsService) {}
+
+  get gifList(): Gif[] {
+    return [...this.gifService.gifList];
+  }
+}
