ce4c4178436765e53f307e13bad6c1f6530cb228
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-27 11:24:18 +0200

    feat: organizeHistory method

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -12,8 +12,17 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
+  private organizeHistory(tag: string): void {
+    tag = tag.toLowerCase();
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((item) => item !== tag);
+    }
+  }
+
   public searchTag(tag: string): void {
     if (tag === '') return;
+
+    this.organizeHistory(tag);
 
     this._tagsHistory.unshift(tag);
   }
