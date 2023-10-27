ed09d54a31111b64e771f198133de6452fa0c8ea
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-28 01:32:57 +0200

    feat: ngOnInit in CardItemComponent to check that there is a gif or throw an error

--- 03-gifs-app/src/app/gifs/components/card-item/card-item.component.ts
+++ 03-gifs-app/src/app/gifs/components/card-item/card-item.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Gif } from '../../interfaces/gifs.interface';
 
 @Component({
   selector: 'gifs-card-item',
   templateUrl: './card-item.component.html',
 })
-export class CardItemComponent {
+export class CardItemComponent implements OnInit {
   @Input()
   public gif!: Gif;
+
+  ngOnInit(): void {
+    if (!this.gif) throw new Error('Gif property is required');
+  }
 }
