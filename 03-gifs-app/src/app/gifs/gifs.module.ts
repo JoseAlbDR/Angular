4afa2b17bb5ac91f135d130e8dffaefbe408e2fa
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-28 18:45:26 +0200

    style: clean unused import

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
 import { ComponentsModule } from './components/components.module';
 
 @NgModule({
