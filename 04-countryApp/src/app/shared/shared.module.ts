142542f70adaa8eab2eb31cadf0b96ad8d7f0f4e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-30 17:54:36 +0100

    feat: about-page component

--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, AboutPageComponent],
   imports: [CommonModule],
-  exports: [HomePageComponent],
+  exports: [HomePageComponent, AboutPageComponent],
 })
 export class SharedModule {}
