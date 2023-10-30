463f07a9ec66275e361b9d613088251a6334790b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-30 17:52:40 +0100

    feat: add home-page component

--- 04-countryApp/src/app/shared/pages/home-page/home-page.component.ts
+++ 04-countryApp/src/app/shared/pages/home-page/home-page.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home-page',
+  selector: 'shared-home-page',
   templateUrl: './home-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
-
-
 @NgModule({
-  declarations: [
-    HomePageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomePageComponent],
+  imports: [CommonModule],
+  exports: [HomePageComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
