8418e5e98b376b97382a36adb5b81ca07c0218f7
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-30 18:28:51 +0100

    feat: contact page component

---
+++ 04-countryApp/src/app/shared/pages/contact-page/contact-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-page',
+  templateUrl: './contact-page.component.html',
+  styles: [
+  ]
+})
+export class ContactPageComponent {
+
+}
--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -5,9 +5,10 @@ import { RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [AboutPageComponent, HomePageComponent, SidebarComponent],
+  declarations: [AboutPageComponent, HomePageComponent, SidebarComponent, ContactPageComponent],
   imports: [CommonModule, RouterModule],
   exports: [HomePageComponent, AboutPageComponent, SidebarComponent],
 })
