2a8bbc6dd55ef35bc71da2cc6a0673b6f198d16b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-30 17:46:14 +0100

    feat: home-page component

---
+++ 04-countryApp/src/app/shared/pages/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styles: [
+  ]
+})
+export class HomePageComponent {
+
+}
--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule
   ]
