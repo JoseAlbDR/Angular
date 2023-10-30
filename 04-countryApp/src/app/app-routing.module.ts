93923826a6bdf31a187f3af5bcea87cfca4547a6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-30 18:27:50 +0100

    feat: sidebar navigation with router and active class

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { AboutPageComponent } from './shared/pages/about-page/about-page.compone
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent,
   },
   {
@@ -15,7 +15,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: '',
   },
 ];
 
--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent],
-  imports: [CommonModule],
-  exports: [HomePageComponent, AboutPageComponent],
+  declarations: [AboutPageComponent, HomePageComponent, SidebarComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HomePageComponent, AboutPageComponent, SidebarComponent],
 })
 export class SharedModule {}
