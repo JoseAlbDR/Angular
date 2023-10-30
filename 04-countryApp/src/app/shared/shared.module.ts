0570d65b7b014664441a894c415c5adb8da66e8e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-30 18:10:47 +0100

    feat: sidebar component

--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent],
+  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent],
   imports: [CommonModule],
   exports: [HomePageComponent, AboutPageComponent],
 })
