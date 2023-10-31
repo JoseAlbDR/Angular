fa9423d10f54073ea6fe954afa91da59a1179366
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-31 09:55:24 +0100

    feat: add countries routes links to sidebar

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -19,6 +19,11 @@ const routes: Routes = [
     component: ContactPageComponent,
   },
   {
+    path: 'countries',
+    loadChildren: () =>
+      import('./countries/countries.module').then((m) => m.CountriesModule),
+  },
+  {
     path: '**',
     redirectTo: '',
   },
--- 04-countryApp/src/app/countries/countries.module.ts
+++ 04-countryApp/src/app/countries/countries.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
-
-
+import { CountriesRoutingModule } from './countries-routing.module';
 
 @NgModule({
   declarations: [
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, CountriesRoutingModule],
 })
-export class CountriesModule { }
+export class CountriesModule {}
