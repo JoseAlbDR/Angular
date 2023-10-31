e32354a6bfd80b312ee1d8c716dd429bc7c5d729
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-31 16:29:16 +0100

    feat: countries table component

---
+++ 04-countryApp/src/app/countries/components/country-table/country-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-country-table',
+  templateUrl: './country-table.component.html',
+  styles: [],
+})
+export class CountryTableComponent {}
