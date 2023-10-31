788e7d3dfde5326df6418d5a9cec957f469949ea
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-31 09:44:13 +0100

    feat: countries routing setup with forChild

--- 04-countryApp/src/app/countries/countries-routing.module.ts
+++ 04-countryApp/src/app/countries/countries-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
-const routes: Routes = [{}];
+const routes: Routes = [
+  {
+    path: 'by-capital',
+    component: ByCapitalPageComponent,
+  },
+  {
+    path: 'by-country',
+    component: ByCountryPageComponent,
+  },
+  {
+    path: 'by-region',
+    component: ByRegionPageComponent,
+  },
+  {
+    path: 'by/:id',
+    component: CountryPageComponent,
+  },
+];
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class CountriesRoutingModule {}
