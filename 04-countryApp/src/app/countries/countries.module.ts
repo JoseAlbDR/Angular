931acf6a38024936b5ee2b939e1a28950867bc73
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-31 09:40:31 +0100

    feat: create all pages needed inside countries

--- 04-countryApp/src/app/countries/countries.module.ts
+++ 04-countryApp/src/app/countries/countries.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 
 
 @NgModule({
   declarations: [
-    ByCapitalPageComponent
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionPageComponent,
+    CountryPageComponent
   ],
   imports: [
     CommonModule
