98098175b2e3264ea9880bb930f495bd2c6a267f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-01 11:39:06 +0100

    fix: minor problems

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Country } from '../interfaces/country';
 import { Observable, catchError, of } from 'rxjs';
 
