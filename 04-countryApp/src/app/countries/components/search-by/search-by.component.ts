0974c95ec2f70d1462e8887fe32b59eb13b33d45
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-02 11:49:47 +0100

    feat: add buttons to available regions and set active class for buttons

--- 04-countryApp/src/app/countries/components/search-by/search-by.component.ts
+++ 04-countryApp/src/app/countries/components/search-by/search-by.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-search-by',
   templateUrl: './search-by.component.html',
@@ -10,6 +12,15 @@ import { CountriesService } from '../../services/countries.service';
 export class SearchByComponent {
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public activeRegion?: Region;
+
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
 
   constructor(private countriesService: CountriesService) {}
 
@@ -18,6 +29,7 @@ export class SearchByComponent {
 
   searchBy(term: string): void {
     this.isLoading = true;
+    this.activeRegion = term as Region;
     const type = this.type.toLowerCase();
     const data = this.countriesService.searchBy(type, term);
     data.subscribe((countries) => {
--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ByRegionPageComponent {
   public type: string = 'Region';
+  
 }
