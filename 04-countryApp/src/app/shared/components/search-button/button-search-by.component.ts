b9520093045ea7f55e2018ca505d9968c34797c3
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-03 12:33:15 +0100

    style: styles for disabled button-search-by component

--- 04-countryApp/src/app/shared/components/search-button/button-search-by.component.ts
+++ 04-countryApp/src/app/shared/components/search-button/button-search-by.component.ts
@@ -1,12 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Region } from '../../../countries/interfaces/region.type';
-import { CountriesService } from '../../../countries/services/countries.service';
-import { Country } from '../../../countries/interfaces/country.interface';
 import { IsLoadingService } from '../../../countries/services/is-loading.service';
 
 @Component({
   selector: 'countries-button-search-by',
   templateUrl: './button-search-by.component.html',
+  styleUrls: ['./button-search-by.component.css'],
 })
 export class SearchButtonComponent {
   constructor(private isLoadingService: IsLoadingService) {}
