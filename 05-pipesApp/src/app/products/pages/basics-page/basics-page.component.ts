2dc456f8f9fb0b5d3269edfef4ba86ad8987f9cf
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-05 01:57:19 +0100

    feat: install tailwind and add some classes to basics-page

--- 05-pipesApp/src/app/products/pages/basics-page/basics-page.component.ts
+++ 05-pipesApp/src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrls: ['./basics-page.component.css']
+  styleUrls: ['./basics-page.component.css'],
 })
 export class BasicsPageComponent {
-
+  public nameLower: string = 'alberto';
+  public nameUpper: string = 'ALBERTO';
+  public fullName: string = 'jOse aLbERtO';
 }
