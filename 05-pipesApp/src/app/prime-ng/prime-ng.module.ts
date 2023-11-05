918e2ac7b8cdafb3ffa3b4ad0049b6dae832e1ae
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-05 01:32:09 +0100

    feat: import needed primeNg components

--- 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -2,8 +2,18 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
-  exports: [ButtonModule, MenuModule, MenubarModule],
+  exports: [
+    ButtonModule,
+    MenuModule,
+    MenubarModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule,
+  ],
 })
 export class PrimeNgModule {}
