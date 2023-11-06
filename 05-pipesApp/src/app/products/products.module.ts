1905f4d2debc1371a5edf7e7202dd943dfd1ae82
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-06 18:00:16 +0100

    fix: style related issues

--- 05-pipesApp/src/app/products/products.module.ts
+++ 05-pipesApp/src/app/products/products.module.ts
@@ -7,10 +7,7 @@ import { NumbersPageComponent } from './pages/numbers-page/numbers-page.componen
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { FormsModule } from '@angular/forms';
-import { TextPipesComponent } from './pages/basics-page/components/text-pipes/text-pipes.component';
-import { DatePipesComponent } from './pages/basics-page/components/date-pipes/date-pipes.component';
 import { BasicsPageModule } from './pages/basics-page/basics-page.module';
-import { NumericPipesComponent } from './pages/numbers-page/components/numeric-pipes/numeric-pipes.component';
 import { NumbersPageModule } from './pages/numbers-page/numbers-page.module';
 
 @NgModule({
