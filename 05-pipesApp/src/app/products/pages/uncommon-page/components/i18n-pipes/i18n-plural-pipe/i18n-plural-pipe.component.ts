21362931f2514d9110a6e5c3338f8de435cc5235
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-08 17:49:39 +0100

    feat: i18n pipes complete

--- 05-pipesApp/src/app/products/pages/uncommon-page/components/i18n-pipes/i18n-plural-pipe/i18n-plural-pipe.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/components/i18n-pipes/i18n-plural-pipe/i18n-plural-pipe.component.ts
@@ -6,6 +6,47 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class I18nPluralPipeComponent {
+  public clients: string[] = [
+    'Maria',
+    'Peter',
+    'John',
+    'Michael',
+    'Ed',
+    'Melissa',
+    'Natalie',
+  ];
+
+  public clientsMap = {
+    '=0': 'no clients waiting',
+    '=1': 'one client waiting',
+    '=2': 'two clients waiting',
+    '=3': 'three clients waiting',
+    '=4': 'four clients waiting',
+    '=5': 'five clients waiting',
+    '=6': 'six clients waiting',
+    '=7': 'seven clients waiting',
+  };
+
+  deleteClient(client = ''): void {
+    if (client) {
+      this.clients = this.clients.filter((name) => client !== name);
+    } else {
+      this.clients.pop();
+    }
+  }
+
+  restoreClients(): void {
+    this.clients = [
+      'Maria',
+      'Peter',
+      'John',
+      'Michael',
+      'Ed',
+      'Melissa',
+      'Natalie',
+    ];
+  }
+
   redirect(url: string) {
     window.open(url, '_blank');
   }
