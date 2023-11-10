a81f1d88d21e1a60107c70e73f4f9ec199411ebf
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-10 09:22:36 +0100

    feat: async pipe complete

--- 05-pipesApp/src/app/products/pages/uncommon-page/components/async-pipe/async-pipe.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/components/async-pipe/async-pipe.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, from, interval, map, tap } from 'rxjs';
 
 @Component({
   selector: 'uncommon-async-pipe',
   templateUrl: './async-pipe.component.html',
   styles: [],
 })
-export class AsyncPipeComponent {}
+export class AsyncPipeComponent implements OnInit {
+  public myObservableTimer = interval(1000).pipe(tap((v) => v));
+
+  public observableTime?: Observable<string>;
+
+  ngOnInit() {
+    this.observableTime = interval(1000).pipe(
+      map(() => {
+        const now = new Date();
+        const hour = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+
+        return `${hour < 10 ? '0' + hour : hour} : ${
+          minutes < 10 ? '0' + minutes : minutes
+        } : ${seconds < 10 ? '0' + seconds : seconds}`;
+      })
+    );
+  }
+}
