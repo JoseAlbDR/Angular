a5eb7388d88047717a835c4db286220bc33bd831
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-12 16:37:39 +0100

    feat: date pipes locale intl

--- 05-pipesApp/src/app/products/pages/basics-page/components/date-pipes-locale/date-pipes-locale.component.ts
+++ 05-pipesApp/src/app/products/pages/basics-page/components/date-pipes-locale/date-pipes-locale.component.ts
@@ -9,5 +9,14 @@ export class DatePipesLocaleComponent {
   @Input()
   public isCollapsed: boolean = true;
 
+  @Input()
+  public title: string = '';
+
+  @Input()
+  public result: string = '';
+
+  @Input()
+  public colTitle: string = '';
+
   public date: Date = new Date();
 }
