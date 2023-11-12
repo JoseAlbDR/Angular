385cd24b3134668ed95b2f3b4be55d5aef754e75
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-12 16:34:24 +0100

    feat: date pipes intl

--- 05-pipesApp/src/app/products/pages/basics-page/components/date-pipes/date-pipes.component.ts
+++ 05-pipesApp/src/app/products/pages/basics-page/components/date-pipes/date-pipes.component.ts
@@ -9,5 +9,11 @@ export class DatePipesComponent {
   @Input()
   public isCollapsed: boolean = true;
 
+  @Input()
+  public title: string = '';
+
+  @Input()
+  public result: string = '';
+
   public date: Date = new Date();
 }
