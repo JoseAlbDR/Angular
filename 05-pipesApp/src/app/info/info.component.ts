baee63b5992f7ea9b11fde43361d8eb86166167d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-12 11:28:20 +0100

    feat: info component intl

--- 05-pipesApp/src/app/info/info.component.ts
+++ 05-pipesApp/src/app/info/info.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { LangService } from '../lang.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit {
+  private languageSubscription?: Subscription;
 
+  constructor(private langService: LangService) {}
+
+  public languageMap = this.langService.infoLanguageMap;
+  public selectedLanguaje = this.langService.getSelectedLanguage;
+
+  ngOnInit(): void {
+    this.languageSubscription = this.langService.languajeChanged.subscribe(
+      (language: string) => {
+        this.selectedLanguaje = language;
+      }
+    );
+  }
 }
